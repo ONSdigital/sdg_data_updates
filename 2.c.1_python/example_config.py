808bbd448e1e335bf2fa76d9756e993475879cf5
Jakki <Varun.Jakki@ons.gov.uk>
2021-06-15 15:23:47 +0100

    Signed-off-by: Jakki <Varun.Jakki@ons.gov.uk>

---
+++ 2.c.1_python/example_config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 15 14:43:40 2021
+
+@author: jakkiv
+"""
+import getpass
+
+download_path = "filepath where files are automatically downloaded to"
+
+output_filepath = "filepath where you want output to be stored"

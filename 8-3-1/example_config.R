87847e0193bcf11452c290a97863cf1cc21e3031
EmmaWoodONS <Emma.Wood@ons.gov.uk>
2021-06-15 14:25:49 +0100

    replace config with example_config
    Signed-off-by: EmmaWoodONS <Emma.Wood@ons.gov.uk>

---
+++ 8-3-1/example_config.R
@@ -0,0 +1,11 @@
+# CONFIGURATIONS for 8.3.1 code
+
+# LOCATION OF DATA
+# replace with relevant filepath for your computer
+filepath <- "parent_filepath_of_all_files"
+year_filepaths <- c("remaining_filepath_to_file_for_a_single_year_starting_with_year_folder.sav",
+                    "remaining_filepath_to_file_for_a_single_year_starting_with_year_folder.sav")
+
+# LOCATION FOR OUTPUT
+output_directory <- "filepath_for_output_location/output" 
+

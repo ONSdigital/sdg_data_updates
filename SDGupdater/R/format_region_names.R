a90480b5429a3236b21e83e443abc8b7c0b9b69a
EmmaWoodONS <Emma.Wood@ons.gov.uk>
2021-06-15 19:00:40 +0100

    SDGupdater branch recreated
    Signed-off-by: EmmaWoodONS <Emma.Wood@ons.gov.uk>

---
+++ SDGupdater/R/format_region_names.R
@@ -0,0 +1,24 @@
+#' @title Format region names
+#'
+#' @description Corrects the capitalisation of region names, and corrects East
+#'   of England to East (which is standard for charts and tables).
+#'
+#' @param variable Character vector of region names
+#'
+#' @return Character vector. Function overwrites region variable with correct
+#'   capitalisation.
+#' @export
+#'
+#' @examples
+#' test_dat <- tibble(Region = c("YORKSHIRE AND THE HUMBER", "EAST OF ENGLAND", "The shire"))
+#' test_dat %>% mutate(Region = format_region_names(Region))
+#'
+#' test_var <- c("YORKSHIRE AND THE HUMBER", "EAST OF ENGLAND", "The shire")
+#' Region <- format_region_names(test_var)
+format_region_names <- function(variable) {
+  dplyr::case_when(variable == "YORKSHIRE AND THE HUMBER" ~ "Yorkshire and The Humber",
+                   variable == "EAST OF ENGLAND" ~ "East",
+                   variable == "Yorkshire and the Humber" ~ "Yorkshire and The Humber",
+                   variable == "East of England" ~ "East",
+                   TRUE ~ stringr::str_to_title(variable))
+}

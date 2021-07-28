086addf7beb4ced9ac307d72acc749fc33802ba0
EmmaWoodONS <Emma.Wood@ons.gov.uk>
2021-07-28 09:35:37 +0100

    added example config file
    Signed-off-by: EmmaWoodONS <Emma.Wood@ons.gov.uk>

---
+++ 15-1-1/example_config.R
@@ -0,0 +1,11 @@
+# configurations for 15-1-1
+
+woodland_filename <- "PWS_2021.xlsx"
+area_filename <- "SAM_CTRY_DEC_2020_UK.csv"
+
+woodland_area_tabname <- "Data for figure 1"
+certified_area_tabname <- "Data for figure 2"
+
+input_folder  <- "Input"
+
+

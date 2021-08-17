c856210e14a6139552c4c8d96138827c3255aebb
EmmaWoodONS <Emma.Wood@ons.gov.uk>
2021-08-17 11:13:08 +0100

    bugfix following review
    My laptop was reading in country as a factor rather than a character which led to differences
    in how the script ran. Country in one df was in uppercase while in the others it
    was in title case so when the dfs were joined, the country data were lost.
    
    All Country columns in all dfs should now be in the same case, regardless of
    whether they are read in as factors or character strings.
    
    Signed-off-by: EmmaWoodONS <Emma.Wood@ons.gov.uk>

--- 15-1-1/example_config.R
+++ 15-1-1/example_config.R
@@ -6,6 +6,6 @@ area_filename <- "SAM_CTRY_DEC_2020_UK.csv"
 woodland_area_tabname <- "Data for figure 1"
 certified_area_tabname <- "Data for figure 2"
 
-input_folder  <- "Input"
+input_folder  <- "Example_Input"
 
 

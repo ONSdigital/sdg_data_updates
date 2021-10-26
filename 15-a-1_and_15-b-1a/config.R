8eea6770603c0df8a70da913d6af99d4072ab41f
ANikolova22 <72549371+ANikolova22@users.noreply.github.com>
2021-10-26 14:36:38 +0100

    Adding config manually as it's in git.ignore

---
+++ 15-a-1_and_15-b-1a/config.R
@@ -0,0 +1,10 @@
+# configurations for 15.a.1/15.b.1
+# save the .ods SID file as a .csv in the Input folder
+# NB: ensure the AmountExtended column (the gross ODA) is formatted as numbers
+# NB: ensure the decimal places are expanded to at least 4dp 
+# (otherwise there may be rounding discrepancies when comparing sums between the R code output and the original ods source file)
+
+
+filename <- "dataunderSID-Final2020.csv"
+
+input_folder  <- "Input"

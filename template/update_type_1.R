2b8975a61e4e833bc26a55fd57913d299489cadc
EmmaWoodONS <emma.wood@ons.gov.uk>
2022-03-28 15:12:23 +0100

    template for simple excel data completed

--- template/update_type_1.R
+++ template/update_type_1.R
@@ -1,15 +1,11 @@
-# EMMA - EDIT THIS FUNCTION IN A NEW BRANCH
-# change the warning given when year/country isn't present
-# it currently says 1: Problem while computing `Year = get_all_years(character)`.
-# i input is of class logical but should be a list 
-# author: Emma Wood
 # date: 24/03/2022
-# THIS IS A TEMPLATE NOT A WORKING SCRIPT
+# THIS IS A TEMPLATE. It may be that not everything is relevant for your data.
+# This script runs on test data so you can look at what everything does line by line
 
 # Type 1 data is simple - one row of column headings and no row names
 # There may or may not be metadata above the column headings - this code allows for both scenarios
 
-# comments can be deleted in your file 
+# Most comments can (should) be deleted in your file 
 
 setwd("template") # this line is to run the template only - do not copy
 
@@ -74,45 +70,15 @@ main_data <- clean_names(main_data)
 
 # remove footnotes -------------------------------------------------------------
 # this assumes that:
-# footnotes might only be in the number of columns stated in check_columns.
+# footnotes are in the first 2 (or whatever number you use as the 
+#   check_columns argument) columns of the datafame.
+#   If there is text in the same row as the footnotes in 
+#   columns beyond check_columns, these rows will not be dropped (see ?remove_footnotes)
 # if the data are likely to have non-footnote data in the first column(s) but NAs
-# in all other columns DO NOT use this function as it will likely remove more 
-# than just footnotes
+#   in all other columns DO NOT use this function as it will likely remove more 
+#   than just footnotes
 
-remove_footnotes <- function(data, check_columns) {
-  
-  column_count <- ncol(data)
-  footnote_na_count <- (column_count - check_columns):column_count
-  
-  data_na_count <-  data %>% 
-    mutate(na_count = rowSums(is.na(data)))
-  
-  for (i in nrow(data_na_count):1) {
-    
-    last_row <- data_na_count[i, ]
-    
-    na_count_end_columns <- sum(is.na(last_row[, (check_columns + 1):column_count]))
-    end_columns_all_NA <- na_count_end_columns  == (column_count - check_columns)
-    
-    if (last_row$na_count %in% footnote_na_count & end_columns_all_NA) {
-      
-      data_na_count <- data_na_count[-i, ]
-      
-    } else {
-      
-      break
-    }
-    
-  }
-  return(data_na_count)
-}
-# count the number of NAs in the bottom row of data
-# if all columns are NA remove bottom row
-# if the first column is not NA, and all other columns are NA remove the bottom row
-# if the 1st and 2nd columns are not NA but the others are remove the bottom row
-# move up a row and continur until this is false
-
-remove_footnotes(main_data, 2)
+main_data <- remove_footnotes(main_data)
 
 # make column names consistent across years ------------------------------------
 
@@ -131,9 +97,11 @@ renamed_main <- main_data %>%
   rename_column(primary = "year", new_name = "year") %>% 
   rename_column(primary = "sex", new_name = "sex") %>% 
   rename_column(primary = c("sample", "size"), alternate = "count", 
-                new_name = "sample_size") %>% 
-  rename_column(primary = "series", not_pattern = "a|b|c|other",
-                new_name = "d")
+                new_name = "sample_size") #%>%
+  # # the following isn't something we want to do here, but to show how you
+  # # match multiple potential patterns using the OR operator:
+  # rename_column(primary = "type", not_pattern = "a|b|c|other",
+  #               new_name = "d")
   
 
 #-------------------------------------------------------------------------------
@@ -146,10 +114,38 @@ renamed_main <- main_data %>%
 # add_row(), filter(), select(), group_by(), summarise()
 # pivot_longer(), pivot_wider()
 
+# If, like in type_1_data, you have multiple columns with values, e.g. where each
+# holds the values for a level of a disaggregation, pivot_longer is your friend
+
+tidy_data <- renamed_main %>% 
+  pivot_longer(
+    cols = c(contains("type"), all_households_000s),
+    names_to = "type",
+    values_to = "value"
+  )
+
+# # for doing calculations, you may rather want values in separate columns, but the same row.
+# # The reverse of pivot_longer is pivot wider. e.g. if I wanted to add types a and b
+# # and the data were already tidy (one value per row)
+# calculation_example <- tidy_data %>% 
+#   pivot_wider(names_from = "type",
+#               values_from = "value") %>% 
+#   mutate(type_a_plus_b = as.numeric(type_a) + as.numeric(type_b))
+# 
+# # then back to tidy:
+# example_tidied <- calculation_example %>% 
+#   pivot_longer(
+#     cols = c(contains("type"), all_households_000s),
+#     names_to = "type",
+#     values_to = "value"
+#   )
+
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
 # finalise csv -----------------------------------------------------------------
+
+# make column names sentence case
 
 # add extra columns for SDMX, rename levels of disaggregations, 
 # put columns in correct order etc 
@@ -157,40 +153,55 @@ renamed_main <- main_data %>%
 # order of disaggregations depend on order they appear. In some cases this won't 
 # be alphanumeric order, so specify the order here and arrange by this fake column 
 # instead of the real one
-age_order <- data.frame(Age = c("Under 15", "16 to 40", "41 to 65", "Over 65"),
-                        Age_order = c(1:4))
+age_order <- data.frame(age = c("Under 66", "Over 65",  ""),
+                        age_order = c(1:3))
 
-csv_formatted <- indicator_data %>% 
+formatted <- tidy_data %>% 
+  # rename columns that need renaming
+  rename(`housing type` = type) %>%
+  # Correct the names of levels of disaggregations, e.g total/UK will nearly always be replaced 
+  # with a blank (""). Use case_when() if there are lots of options, or ifelse if there is just one
+  mutate(age = 
+           case_when(
+             age == "all" ~ "",
+             age == "< 66" ~ "Under 66",
+             age == "> 65" ~ "Over 65",
+             # this last line says 'for all other cases, keep Age the way it is
+             TRUE ~ as.character(age)),
+         sex = ifelse(sex == "All", "", sex),
+         # totals should be blank, not e.g. 'all'
+         `housing type` = ifelse(`housing type` == "all_households_000s", "", `housing type`),
+         # If value is NA give a reason for why it is blank (as below) or...
+         `observation status` = ifelse(is.na(value), "Missing value", "Normal value")
+         ) %>% 
+  # you can also use pattern matching to change level names, e.g. removing the 
+  # word 'type' fromthe housing types column
+  mutate(`housing type` = stringr::str_replace(`housing type`,"type_|_types",  "")) %>% 
+  # add columns that don't exist yet (e.g. for SDMX)
+  # (you need backticks if the column name has spaces)
+  mutate(units = "Number",
+         `unit multiplier` = "Thousands") %>% 
+  # ... or remove it using filter() as the commented line below
+  # filter(!is.na(Value)) %>%
   # we changed everything to lowercase at the top of the script, 
   # so now we need to change them back to the correct case
-  mutate(Country = str_to_title(Country)) %>% 
-  # we also changed column names to lowercase, so we need to change them back again too
-  # note that column names are changed with rename(), 
-  # while contents of columns are changed with mutate()
-  rename(Year = year) %>% 
-  # rename levels of disaggregations, e.g total/UK will nearly always be replaced 
-  # with a blank. Use case_when() if there are lots of options, or ifelse if there is just one
-  mutate(Age = 
-           case_when(
-             Age == "<15" ~ "Under 15",
-             Age == ">65" ~ "Over 65",
-             # this last line says 'for all other cases, keep Age the way it is
-             TRUE ~ as.character(Age)),
-         Country = ifelse(Country == "UK", "", Country)) %>% 
-  # Remove any rows where the value is NA
-  filter(!is.na(Value)) %>% 
+  mutate(across(where(is.character), str_to_sentence)) %>% 
+  # if you then have to change country as well:
+  # mutate(Country = str_to_title(Country)) %>% 
+  
   # order of disaggregations depend on order they appear, so sort these now
-  left_join(age_order, by = "Age") %>% 
-  arrange(Year, Country, Age_order) %>% 
-  # Add extra rows required for SDMX
-  mutate(`Observation status` = "Undefined",
-         `Unit multiplier` = "Units",
-         `Unit measure` = "percentage (%)") %>% 
+  # arrange will put them in alphanumeric order, so if you dont want these follow the age example here
+  left_join(age_order, by = "age") %>% 
+  arrange(year, age_order, sex) %>% 
   # Put columns in the order we want them.
-  # this also gets rid of the column Age_order which has served its purpose and is no longer needed
-  select(Year, Country, Age, 
-         `Observation status`, `Unit measure`, `Unit multiplier`,
-         Value)
+  # this also gets rid of the column age_order which has served its purpose and is no longer needed
+  select(year, `housing type`, age, sex, 
+         `observation status`, `units`, `unit multiplier`,
+         value)
+
+# finally, put the column names in sentence case
+names(formatted) <- str_to_sentence(names(formatted))
+
 
 
 

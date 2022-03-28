93a78a0d1c0db64bde335ccb91f531cb7c4bfec7
EmmaWoodONS <emma.wood@ons.gov.uk>
2022-03-28 12:19:04 +0100

    tests for remove_footnotes

--- SDGupdater/R/remove_footnotes.R
+++ SDGupdater/R/remove_footnotes.R
@@ -12,9 +12,9 @@
 #' If the data are likely to have non-footnote (real) data in the first x 
 #' column(s) and NAs in all other columns DO NOT use this function as it will 
 #' likely remove more than just footnotes
-#'
-#' @param data dataframe or tibble.
-#' @param check_columns numeric. The maximum index in which you expect there to be footnotes
+#' 
+#' @param data dataframe with footnotes in which the footnote is followed by NA cells on the same row.
+#' @param check_columns numeric. The maximum column index in which you expect there to be footnotes. Default is 2.
 #' @return data frame with footnote, and all-NA rows removed
 #'
 #' @examples
@@ -27,33 +27,48 @@
 #'                         
 #' @export
 
-remove_footnotes <- function(data, check_columns) {
+remove_footnotes <- function(data, check_columns=2) {
   
-  column_count <- ncol(data)
-  footnote_na_count <- (column_count - check_columns):column_count
-  
-  data_na_count <-  data %>% 
-    mutate(na_count = rowSums(is.na(data)))
-  
-  for (i in nrow(data_na_count):1) {
+  if(!is.data.frame(data)) {
+    stop("data must be a dataframe")
+  } else {
+    column_count <- ncol(data)
     
-    last_row <- data_na_count[i, ]
-    
-    na_count_end_columns <- sum(is.na(last_row[, (check_columns + 1):column_count]))
-    end_columns_all_NA <- na_count_end_columns  == (column_count - check_columns)
-    
-    if (last_row$na_count %in% footnote_na_count & end_columns_all_NA) {
-      
-      data_na_count <- data_na_count[-i, ]
-      
-    } else {
-      
-      break
+    if(check_columns == column_count) {
+      warning("check_columns should be less than the number of columns in the dataframe. 
+            \nNo changes have been made as check_columns == ncol(data)")
     }
     
+    if(check_columns > column_count) {
+      
+      stop("check_columns must be less than the number of columns in the dataframe")
+      
+    } else {
+      footnote_na_count <- (column_count - check_columns):column_count
+      
+      data_na_count <-  dplyr::mutate(data, na_count = rowSums(is.na(data)))
+      
+      for(i in nrow(data_na_count):1) {
+        
+        last_row <- data_na_count[i, ]
+        
+        na_count_end_columns <- sum(is.na(last_row[, (check_columns + 1):column_count]))
+        end_columns_all_NA <- na_count_end_columns  == (column_count - check_columns)
+        
+        if (last_row$na_count %in% footnote_na_count & end_columns_all_NA) {
+          
+          data_na_count <- data_na_count[-i, ]
+          
+        } else {
+          
+          break
+        }
+        
+      }
+      
+      no_footnotes <- dplyr::select(data_na_count, -na_count)
+      
+      return(no_footnotes)
+    }
   }
-  
-  no_footnotes <- select(data_na_count, -na_count)
-  
-  return(no_footnotes)
 }

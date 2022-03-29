54ffd3331010cfe1bdbace58ba43fc873918dc01
Atanaska <atanaska.nikolova@ons.gov.uk>
2022-03-29 15:32:22 +0100

    Edited code to fit template

---
+++ 9-a-1/compile_tables.R
@@ -0,0 +1,41 @@
+# This file is directly called by update_indicator_main.R.
+# It has to be called compile_tables.R
+# It is the control script that runs all the others.
+
+
+
+source("config.R") # pulls in all the configurations
+source("update_9-a-1.R") # does the donkey-work of making the csv
+
+# Using the sourced function, we run it on the two input files to get the final data for formatting
+ind_9_a_1_200916 <- ODA_9.a.1(data_underlying_SID = read.csv("filename_1.csv", header=TRUE)) 
+ind_9_a_1_201720 <- ODA_9.a.1(data_underlying_SID = read.csv("filename_2.csv", header=TRUE))
+
+
+csv_formatted <- rbind(ind_9_a_1_200916, ind_9_a_1_201720) 
+csv_formatted <- csv_formatted[order(csv_formatted$`Country income classification`),]
+
+
+
+existing_files <- list.files()
+output_folder_exists <- ifelse(output_folder %in% existing_files, TRUE, FALSE)
+
+if (output_folder_exists == FALSE) {
+  dir.create(output_folder)
+}
+
+date <- Sys.Date()
+
+# we add the date to the output file so that old outputs are not automatically overwritten.
+# However, it shouldn't matter if they are overwritten, as files can easily be recreated with the code.
+# We may want to review the decision to add date to the filename.
+csv_filename <- paste0(date, "_update_9-a-1.csv")
+
+write.csv(csv_formatted, paste0(output_folder, "/", csv_filename), row.names = FALSE)
+
+
+message(paste0("The csv file has been created and saved in '", paste0(getwd(), "/", output_folder, "'"),
+               " as ", csv_filename))
+
+# so we end on the same directory as we started before update_indicator_main.R was run:
+setwd("..")
---
+++ 9-a-1/update_9-a-1.R
@@ -0,0 +1,56 @@
+# date: 29/03/2022
+# Author: Atanaska Nikolova
+
+
+
+# The code is contained in one large function that does everything
+# It is then called from compile_tables.R
+
+ODA_9.a.1 <- function(data_underlying_SID){
+  colnames(data_underlying_SID)[ncol(data_underlying_SID)] <- "ODA"
+  
+  # Renaming the SIDsector column to have a consistent name 
+  # because it differs slightly between the two csv spreadsheets
+  rows_to_rename <- which(grepl("Economic Infrastructure",data_underlying_SID$SIDsector))
+  data_underlying_SID$SIDsector[rows_to_rename] <- "Economic Infrastructure" 
+  
+  # This is the SIDsector variable by which we filter the data
+  SIDsector = "Economic Infrastructure"
+  
+  infrastructure <- aggregate(data_underlying_SID$ODA,
+                              by = list(Year = data_underlying_SID$Year, 
+                                        IncomeGroup = data_underlying_SID$IncomeGroup,
+                                        SID_sector = data_underlying_SID$SIDsector==SIDsector), 
+                              FUN = sum)
+  infrastructure <- infrastructure[infrastructure$SID_sector == TRUE,]
+  infrastructure[infrastructure$SID_sector == TRUE,"SID_sector"] <- "Economic Infrastructure and Services"
+  colnames(infrastructure)[4] <- "Value"
+  infrastructure[(infrastructure$IncomeGroup == "0" | infrastructure$IncomeGroup == "Part I unallocated by income"),
+                 "IncomeGroup"] <- "Unspecified"
+  
+  headline<-aggregate(infrastructure$Value,
+                      by = list(Year = infrastructure$Year),
+                      FUN = sum)
+  headline$IncomeGroup <- ""
+  headline$SID_sector <- "Economic Infrastructure and Services"
+  headline$Value <- headline$x
+  headline <- headline[,-2]
+  
+  infrastructure <- rbind(infrastructure, headline)
+  infrastructure$`Observation status` <- "Definition differs"
+  infrastructure$`Unit multiplier` <- "Thousands"
+  names(infrastructure)[names(infrastructure) == 'IncomeGroup'] <- 'Country income classification'
+  infrastructure$Units <- "GBP (£ thousands)"
+  
+  # we don't need the SID sector column anymore, so removing it
+  infrastructure <- infrastructure[,-3]
+  #reordering the columns to make compatible with csv
+  infrastructure <- infrastructure[, c(1, 2, 4, 5, 6, 3)]
+  infrastructure <- infrastructure[order(infrastructure$`Country income classification`),]
+  
+  return(infrastructure)
+}
+
+
+
+

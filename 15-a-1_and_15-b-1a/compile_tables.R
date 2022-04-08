d0a00e37d85fdc945a8fb27cfbb21fdafdbd29a2
Atanaska <atanaska.nikolova@ons.gov.uk>
2022-04-08 08:59:08 +0100

    Updated code after review + output example

--- 15-a-1_and_15-b-1a/compile_tables.R
+++ 15-a-1_and_15-b-1a/compile_tables.R
@@ -13,15 +13,17 @@ if (output_folder_exists == FALSE) {
   dir.create("Output")
 }
 
-first_input_file <- ODA_15.a.1(data_underlying_SID = read.csv(paste0(getwd(),"/",input_folder,"/",filename_1), 
-                                                           header=T))
-second_input_file <- ODA_15.a.1(data_underlying_SID = read.csv(paste0(getwd(),"/",input_folder,"/",filename_2), 
-                                                              header=T))
-csv_formatted <- rbind(first_input_file, second_input_file)
-csv_formatted$Units <- "GBP (£ Millions)"
+first_file <- read.csv(paste0(getwd(), "/", input_folder, "/", filename_1), header = TRUE)
+second_file <- read.csv(paste0(getwd(), "/", input_folder, "/", filename_2), header = TRUE)
+                                               
+biodiversity_values_file_1 <- ODA_15.a.1(data_underlying_SID = first_file)
+biodiversity_values_file_2 <- ODA_15.a.1(data_underlying_SID = second_file)
+
+csv_formatted <- rbind(biodiversity_values_file_1, biodiversity_values_file_2)
+csv_formatted$Units <- "GBP (Â£ Millions)"
 csv_formatted$Series <- "Total official development assistance for biodiversity, by recipient countries"
-csv_formatted$UnitMultiplier <- "Millions"
-csv_formatted$ObservationStatus <- "Normal value"
+csv_formatted$`Unit multiplier` <- "Millions"
+csv_formatted$`Observation status` <- "Normal value"
 
 #reordering the columns to make compatible with csv ordering
 csv_formatted <- csv_formatted[, c(1, 5, 2, 4, 6, 7, 3)]
@@ -31,10 +33,10 @@ csv_formatted <- csv_formatted[order(csv_formatted$Year, csv_formatted$`Country 
 date <- Sys.Date()
 csv_filename <- paste0(date, "_15.a.1a_15.b.1a.csv")
 
-
 write.csv(csv_formatted, paste0("Output/", csv_filename), row.names = FALSE)
 
-
+message(paste0("The csv has been created and saved in '", paste0(getwd(), "/Output", "'"),
+               " as ", csv_filename))
 
 # so we end on the same directory as we started before update_indicator_main.R was run:
 setwd("..")
--- 15-a-1_and_15-b-1a/update_15.a.1a_15.b.1a.R
+++ 15-a-1_and_15-b-1a/update_15.a.1a_15.b.1a.R
@@ -7,24 +7,35 @@
 
 ODA_15.a.1 <- function(data_underlying_SID){
   CRScode <- 41030
-  colnames(data_underlying_SID)[ncol(data_underlying_SID)-2] <- "GrossODA"
   
-  # removing blank cells for AmountExtended - equalising to 0 to avoid NAs
+  # putting the data frame columns in lower case and renaming the key column for the sums,
+  # so if it changes order in the source file the code won't get affected
+  names(data_underlying_SID) <- tolower(names(data_underlying_SID))
+  data_underlying_SID <- rename_column(dat = data_underlying_SID,
+                primary = "amountsextended",
+                alternate = "extended",
+                new_name = "GrossODA")
+ 
+  
+  # removing blank cells for GrossODA - equalising to 0 to avoid NAs
   data_underlying_SID[which(is.na(data_underlying_SID$GrossODA)),"GrossODA"] <- 0
   
   biodiversity <- aggregate(data_underlying_SID$GrossODA,
-                            by = list(Year = data_underlying_SID$Year, 
-                                      `Country income classification` = data_underlying_SID$IncomeGroup,
-                                      CRS_code = data_underlying_SID$SectorPurposeCode.CRScode. == CRScode), 
+                            by = list(Year = data_underlying_SID$year, 
+                                      `Country income classification` = data_underlying_SID$incomegroup,
+                                      CRS_code = data_underlying_SID$sectorpurposecode.crscode. == CRScode), 
                             FUN = sum)
   biodiversity <- biodiversity[biodiversity$CRS_code == TRUE,]
   biodiversity <- biodiversity[ , -3]
   
-  colnames(biodiversity)[3] <- "Value"
+  colnames(biodiversity)[which(names(biodiversity) == "x")] <- "Value"
   
   # grouping up income group that doesn't follow official DAC into "Undefined" for consistency
-  biodiversity[(biodiversity$`Country income classification` == "0" | biodiversity$`Country income classification` == "Part I unallocated by income"),
-               "Country income classification"] <- "Unspecified"
+  biodiversity[(biodiversity$`Country income classification` == 
+                  "0" | biodiversity$`Country income classification` == 
+                  "Part I unallocated by income"),
+                  "Country income classification"] <- "Unspecified"
+  
   # adding totals to the headline and combine with the final table
   headline <- aggregate(biodiversity$`Value`, 
                         by=list(Year = biodiversity$Year),

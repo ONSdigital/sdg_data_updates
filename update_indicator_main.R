45b6c685823edafc868ed426b62c4a9d10af1154
EmmaWoodONS <emma.wood@ons.gov.uk>
2022-05-05 18:59:37 +0100

    indicator change

--- update_indicator_main.R
+++ update_indicator_main.R
@@ -23,7 +23,7 @@ library(stringr)
 library(SDGupdater)
 
 
-indicator <- "15-a-1_and_15-b-1a" # name of folder for indicator
+indicator <- "3-c-1" # name of folder for indicator
 
 
 setwd(indicator)

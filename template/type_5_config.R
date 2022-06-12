f78c74f0101d9eb0f1b4a3c219e62ad464834c7c
Wood <emma.wood@ons.gov.uk>
2022-06-12 17:00:05 +0100

    most of template 5 code done

---
+++ template/type_5_config.R
@@ -0,0 +1,17 @@
+# configurations for type 5 (ONS CDID) data
+
+# link for data - see the readme
+# ONS CDID (series ID) link generation instructions should be explained in the README file
+#   i.e. which selections to use to generate the link. 
+
+# in some cases where CDIDs are available using the excel publication 
+# (template 1 or 2) may be better, for example if there are a lot of series.
+
+
+# csv link for the series GLBK
+expenditure_link <- "https://www.ons.gov.uk/generator?format=csv&uri=/economy/governmentpublicsectorandtaxes/researchanddevelopmentexpenditure/timeseries/glbk/gerd"
+
+# csv link for GDP from the Business enterprise research and development time series (BERD)
+gdp_link <- "https://www.ons.gov.uk/generator?format=csv&uri=/economy/governmentpublicsectorandtaxes/researchanddevelopmentexpenditure/timeseries/dlht/berd"
+
+output_folder <- "Example_output"

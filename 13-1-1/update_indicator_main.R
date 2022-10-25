8e725cd33d1f7277e216534584c4bf26c135b133
Steven Jones <steven.jones@ons.gov.uk>
2022-10-25 12:03:28 +0100

    first attempt, spaghetti functions for aggregates

---
+++ 13-1-1/update_indicator_main.R
@@ -0,0 +1,26 @@
+# Author: Emma Wood
+# Date: 08/01/2021
+# Purpose: To create csv data for indicators 3.2.2, ...
+# Requirements: This script runs the code in the folder stated by indicator <- "indicator_folder_name" below. 
+# Runtime: last run approx. 22 seconds
+
+# You should not need to install any packages, but if you do, use the following 
+# code and just replace the name of the package from tidyr to the package you need.
+# install.packages("tidyr", dependencies = TRUE, type = "win.binary")
+
+# Because SDGupdater is a local package we install it slightly differently:
+# setwd("D:/Coding_repos/sdg_data_updates")
+# install.packages("SDGupdater", repos = NULL, type="source", force = TRUE)
+rm(list = ls())
+
+
+indicator <- "13-1-1" # name of folder for indicator
+
+
+setwd(indicator)
+
+source("compile_tables.R")
+
+
+
+

0148cdf804cd89eb961cff581b0bb0f119246ffe
Nairn <Michael.Nairn@ons.gov.uk>
2022-11-03 15:59:06 +0000

    Michael review added

--- 11-1-1/example_config.R
+++ 11-1-1/example_config.R
@@ -9,5 +9,5 @@ households_header_row <- 5
 tabnames <- c("2019", 
               "2020")
 
-input_folder  <- "Example_input"
-output_folder  <- "Example_output"
+input_folder  <- "Input"
+output_folder  <- "Output"

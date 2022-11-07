9e320b790d0c09944599ad0a12b1f1a5e1cd2fba
Wood <emma.wood@ons.gov.uk>
2022-11-07 16:24:19 +0000

    add 16-3-2 example input and output

--- 16-3-2/compile_tables.R
+++ 16-3-2/compile_tables.R
@@ -40,14 +40,15 @@ csv <- bind_rows(csv_sex_age, csv_nationality) %>%
   distinct()
 
 existing_files <- list.files()
-if("Output" %not_in% existing_files) {
-  dir.create("Output")
+if(output_folder %not_in% existing_files) {
+  dir.create(output_folder)
 }
 
 
-write.csv(csv, paste0("Output/output_", run_date, ".csv"), row.names = FALSE)
+write.csv(csv, paste0(output_folder, "/output_", run_date, ".csv"), row.names = FALSE)
 
-rmarkdown::render('16-3-2_checks.Rmd', output_file = 'Output/16-3-2_checks.html')
+rmarkdown::render('16-3-2_checks.Rmd', 
+                  output_file = paste0(output_folder, '/16-3-2_checks.html'))
 
 
 setwd('..')

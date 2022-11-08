8f96ba11f6e7ba23efa962efd58b6b8f81a00c65
EmmaWoodONS <emma.wood@ons.gov.uk>
2022-11-08 16:33:29 +0000

    add 15-a-1 QA plots
    while adding plots, fixes to 15-a-1 script made so it runs series a even if oecd api doesn't work

--- ODA/15-a-1_15-b-1.R
+++ ODA/15-a-1_15-b-1.R
@@ -10,7 +10,8 @@ headline <- biodiversity %>%
 
 gbp_data <- bind_rows(biodiversity, 
                       headline) %>% 
-  mutate(Units = "GBP (£ thousands)",
+  mutate(value = value/1000) %>% 
+  mutate(Units = "GBP (£ Millions)",
          country_income_classification = ifelse(
            grepl("unallocated", country_income_classification)== TRUE |
              country_income_classification == "0",
@@ -28,36 +29,45 @@ series_a <- gbp_data %>%
          `Observation status`, Units, Value) 
 
 # Series using data specific to 15-a-1/15-b-1 (15-a-1b/15-b-1b) ----
-series_b <- biodiversity_data %>% 
-  mutate(
-    Units = case_when(
-      VAR == "BASE_NC" ~ "GBP (£ Millions)",
-      VAR == "BASE_USD" ~ "USD ($ Millions)",
-      VAR == "BASE_REV" ~ "Percentage (%)",
-      VAR == "BASE_GDP" ~ "Percent of GDP (%)"
-    )) %>% 
-  mutate(
-    Series = case_when(
-      VAR == "BASE_NC" | VAR == "BASE_USD" ~ "Tax revenue from biodiversity-relevant taxes",
-      VAR == "BASE_REV" ~ "Tax revenue from biodiversity-relevant taxes out of total tax revenue",
-      VAR == "BASE_GDP" ~ "Tax revenue from biodiversity-relevant taxes"
-    ),
-    `Observation status` = "Estimated value",
-    obsTime = as.integer(obsTime)) %>% 
-  # Obs status is stated as 'lower end estimate' in the OECD dataset documentation 
-  # once the selections are made as described in source 2 of 15-a-1. To read, 
-  # go to Export > Related files
-  rename(Year = obsTime,
-         Value = obsValue) %>% 
-  select(Year, Series, `Observation status`, Units, Value)
+if ("biodiversity_data" %in% objects()){
+  series_b <- biodiversity_data %>% 
+    mutate(
+      Units = case_when(
+        VAR == "BASE_NC" ~ "GBP (£ Millions)",
+        VAR == "BASE_USD" ~ "USD ($ Millions)",
+        VAR == "BASE_REV" ~ "Percentage (%)",
+        VAR == "BASE_GDP" ~ "Percent of GDP (%)"
+      )) %>% 
+    mutate(
+      Series = case_when(
+        VAR == "BASE_NC" | VAR == "BASE_USD" ~ "Tax revenue from biodiversity-relevant taxes",
+        VAR == "BASE_REV" ~ "Tax revenue from biodiversity-relevant taxes out of total tax revenue",
+        VAR == "BASE_GDP" ~ "Tax revenue from biodiversity-relevant taxes"
+      ),
+      `Observation status` = "Estimated value",
+      obsTime = as.integer(obsTime)) %>% 
+    # Obs status is stated as 'lower end estimate' in the OECD dataset documentation 
+    # once the selections are made as described in source 2 of 15-a-1. To read, 
+    # go to Export > Related files
+    rename(Year = obsTime,
+           Value = obsValue) %>% 
+    select(Year, Series, `Observation status`, Units, Value)
+  
+  # Bind data for both series ----
+  csv <- bind_rows(series_a, series_b) %>% 
+    arrange(Year, Country_income_classification) %>% 
+    replace(is.na(.), "") %>% 
+    rename(`Country income classification` = Country_income_classification)
+  
+  rm(biodiversity, series_b)  
 
-# Bind data for both series ----
-csv <- bind_rows(series_a, series_b) %>% 
+  }
+
+csv <- series_a %>% 
   arrange(Year, Country_income_classification) %>% 
   replace(is.na(.), "") %>% 
   rename(`Country income classification` = Country_income_classification)
 
-rm(biodiversity, headline, gbp_data, 
-   series_a, seroes_b)
+rm(headline, gbp_data, series_a)
 
 scripts_run <- c(scripts_run, "15-a-1_15-b-1")

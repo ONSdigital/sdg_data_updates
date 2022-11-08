20370a2c40bb165e1a31fcdac1df69617c648913
Nairn <Michael.Nairn@ons.gov.uk>
2022-11-08 11:27:39 +0000

    5-a-1 script finished

--- 5-a-1/5-a-1.R
+++ 5-a-1/5-a-1.R
@@ -1,34 +1,34 @@
-# date: 12/04/2022
-# THIS IS A TEMPLATE. It may be that not everything is relevant for your data.
-# This script runs on test data so you can look at what everything does line by line
+#### Michael Nairn automation code for SDG Indicator 5-a-1 ####
 
-# Type 4 data is data from Nomis. It does not require a manual download as it 
+# 8th November 2022 
+
+# Data from Nomis. It does not require a manual download as it 
 # has a stable weblink (API is also available here but not used as weblink is 
 # more straightforward)
-  
-# indicators often require population numbers as the denominator, so this 
-# template is based on an indicator for which the numerator is some series from
-# nomis divided by population (also from nomis).
 
-# Most comments can (should) be deleted in your file, and data renamed as approriate 
+
+# This indicator asks for Proportion of total agricultural population with
+ # ownership or secure rights over agricultural land, by sex
+
+# Therefore need three datasets: Male, Female, total population
+
 
 # download and read in data ----------------------------------------------------
-numerator_data <- read.csv(numerator_link) %>% 
+NOMIS_data <- read.csv(NOMIS_data) %>% 
   mutate(across(where(is.factor), as.character)) %>% 
   mutate(across(where(is.character), str_to_sentence)) %>% 
   mutate(across(where(is.character), str_squish)) 
 
-population_data <- read.csv(population_link) %>% 
-  mutate(across(where(is.factor), as.character)) %>% 
-  mutate(across(where(is.character), str_to_sentence)) %>% 
-  mutate(across(where(is.character), str_squish)) 
-  
+
+#### Ensure annual (Jan-Dec) data, as opposed to quarters #### 
+
 # get the months the years run from and to (to use in QA)- this can be removed
 # if data are only published annually. It is included for data published more 
 # frequently as a check that the right data have been pulled from nomis 
-months <- as.character(unique(numerator_data$DATE_NAME))
+months <- as.character(unique(NOMIS_data$DATE_NAME))
 months_no_years <- gsub('[[:digit:]]+', '', months)
 unique_months <- unique(months_no_years)
+
 
 # when running back series, we could download just the annual data so that we can download more years at once.
 # However, for standard updates we should only need to download the last two years at the most.
@@ -46,80 +46,36 @@ if(multiple_quarters == TRUE) {
     select(-keep_quarter)
 }
 
-# clean up the numerator data---------------------------------------------------
-clean_numerator <- numerator_annual %>% 
-  # we don't need confidence intervals - though this may change in future
-  filter(MEASURES_NAME == "Value") %>% 
+
+
+#### Clean up data ####
+
+NOMIS_clean <- NOMIS_data %>% 
+  # we don't need confidence intervals or rates - though this may change in future
+  filter(MEASURES_NAME == "Value" & MEASURE_NAME == "Count") %>% 
   # split the geographies so country and region are in separate columns
-  # and (for this example, we also split the occupation codes into minor group and unit group 
   mutate(
     Country = case_when(
       GEOGRAPHY_TYPE == "Countries" ~ GEOGRAPHY_NAME, 
       GEOGRAPHY_TYPE == "Regions" ~ "England",
       TRUE ~ ""),
-    Region = ifelse(GEOGRAPHY_TYPE == "Regions", GEOGRAPHY_NAME, ""),
-    # while C_OCCPUK11H_0_NAME currenlty gives the code and the name in one string, 
-    # in the next step we fill this column with just the code, so calling this 
-    # minor_group_code not `Occupation minor group`
-    minor_group_code = ifelse(C_OCCPUK11H_0_TYPE == "3-digit occupation", C_OCCPUK11H_0_NAME,""),
-    `Occupation unit group` = ifelse(C_OCCPUK11H_0_TYPE == "4-digit occupation",  C_OCCPUK11H_0_NAME, "")) %>% 
-  rename(numerator = OBS_VALUE,
-         `Observation status` = OBS_STATUS_NAME) %>% 
-  select(DATE_CODE, 
-         Country, Region, 
-         minor_group_code, `Occupation unit group`, 
-         GEOGRAPHY_CODE,
-         `Observation status`,
-         numerator)
-
-# This example uses occupation codes. For our csvs this kind of data is split
-# out into a nested disaggregation - with occupation unit group nested under 
-# occupation minor group. In the nomis data, all occupation codes are in a single 
-# column. We want them in two columns. In order to make sure the minor group has the 
-# correct value when there is a unit group (i.e. not a minor group total), we first 
-# create a lookup table:
-occupation_lookup <- numerator_annual %>%
-  filter(C_OCCPUK11H_0_TYPE == "3-digit occupation") %>%
-  distinct(C_OCCPUK11H_0_NAME) %>%
-  mutate(CODE = substring(C_OCCPUK11H_0_NAME, 1, 3))
-
-# Fill in Occupation minor group column
-occupations_filled <- clean_numerator %>% 
-  # the first three numbers of the unit group tell you what the minor group is
-  mutate(
-    minor_group_code = case_when(
-      `Occupation unit group` != "" ~ substr(`Occupation unit group`, 1, 3),
-      `Occupation unit group` == "" ~ substr(minor_group_code, 1, 3),
-      TRUE ~ as.character(minor_group_code))
-    )%>%
-  left_join(occupation_lookup, by = c("minor_group_code" = "CODE")) %>% 
-  rename(`Occupation minor group` = C_OCCPUK11H_0_NAME) %>% 
-  select(-minor_group_code)
+    Region = ifelse(GEOGRAPHY_TYPE == "Regions", GEOGRAPHY_NAME, "")) %>%
+  # select relevant columns
+  select(DATE_CODE,
+             Country,
+             Region,
+             C_SEX_NAME,
+             MEASURE_NAME,
+             MEASURES_NAME,
+             OBS_VALUE,
+             OBS_STATUS_NAME) %>% 
+      # rename columns for output
+      rename(Sex = C_SEX_NAME,
+             `Observation status` = OBS_STATUS_NAME)
 
 
-# clean up the denominator data-------------------------------------------------
 
-clean_population <- population_data %>% 
-  # not all data will be relevant so filter it for what you need
-  filter(MEASURES_NAME == "Value" & # don't need confidence intervals
-           # the numerator data isn't disaggregated by sex or age, so only need the totals
-           SEX_NAME == "Total" &
-           AGE_NAME == "All ages") %>%  
-  # country and region need to be in separate columns
-  mutate(Country = case_when(
-    GEOGRAPHY_TYPE == "Countries" ~ GEOGRAPHY_NAME, 
-    GEOGRAPHY_TYPE == "Regions" ~ "England",
-    TRUE ~ ""),
-    Region = ifelse(GEOGRAPHY_TYPE == "Regions", GEOGRAPHY_NAME, "")) %>% 
-  rename(population = OBS_VALUE) %>% 
-  # make date a string as the date column you later use to join the two 
-  # dataframes is a character string
-  mutate(DATE_CODE = as.character(DATE_CODE)) %>% 
-  # make sure you have filtered for only things you need and that you have selected 
-  # all the relevant columns. 
-  select(DATE_CODE, 
-         Country, Region, 
-         population)
+
 
 # This is a line that you can run to check that you have filtered and selected 
 # correctly - all rows in the clean_population dataframe should be unique

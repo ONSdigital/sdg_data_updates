d12bf853e987cebeec8153143c9064cbd324ce05
Katie Uzzell <katie.uzzell@ons.gov.uk>
2023-01-04 11:34:51 +0000

    fixes to QA file graph

--- 15-7-1_and_15-c-1/update_15-7-1_and_15-c-1.R
+++ 15-7-1_and_15-c-1/update_15-7-1_and_15-c-1.R
@@ -18,15 +18,15 @@ names(seizures_main_data) <- SDGupdater::remove_superscripts(names(seizures_main
 # rename columns
 
 renamed_seizures_main_data <- seizures_main_data %>% 
-  rename_column(primary = "Number of Times seized: Caviar & Caviar extract", new_name = "Caviar & Caviar extract") %>% 
-  rename_column(primary = "Number of Times seized: Live Coral & Coral Derivatives", new_name = "Live Coral & Coral Derivatives") %>% 
+  rename_column(primary = "Number of Times seized: Caviar & Caviar extract", new_name = "Caviar and Caviar extract") %>% 
+  rename_column(primary = "Number of Times seized: Live Coral & Coral Derivatives", new_name = "Live Coral and Coral Derivatives") %>% 
   rename_column(primary = "Number of Times seized: Ivory and Items Containing Ivory", new_name = "Ivory and Items Containing Ivory") %>% 
   rename_column(primary = "Number of Times seized: Live Animals and Birds", new_name = "Live Animals and Birds") %>%
   rename_column(primary = "Number of Times seized: Live Plants", new_name = "Live Plants") %>%
   rename_column(primary = "Number of Times seized: Parts or Derivatives of Animals/Birds", new_name = "Parts or Derivatives of Animals/Birds") %>%
   rename_column(primary = "Number of Times seized: Parts or Derivatives of Plants", new_name = "Parts or Derivatives of Plants") %>%
   rename_column(primary = "Number of Times seized: Timber or Wood Products", new_name = "Timber or Wood Products") %>%
-  rename_column(primary = "Number of Times seized: Preparations Of Oriental Medicine Which Include Parts & Derivatives Of Endangered Species", new_name = "Preparations Of Oriental Medicine Which Include Parts & Derivatives Of Endangered Species") %>%
+  rename_column(primary = "Number of Times seized: Preparations Of Oriental Medicine Which Include Parts & Derivatives Of Endangered Species", new_name = "Preparations Of Oriental Medicine Which Include Parts and Derivatives Of Endangered Species") %>%
   rename_column(primary = "Number of Times seized: Butterflies", new_name = "Butterflies") 
 
 # split year and quarter column into two
@@ -36,7 +36,7 @@ renamed_seizures_main_data[c('Year', 'Quarter')] <- str_split_fixed(renamed_seiz
 # select wanted columns
 
 renamed_seizures_main_data <- renamed_seizures_main_data %>% 
-  select(c("Year", "Quarter", "Total Number of Seizures", "Caviar & Caviar extract", "Live Coral & Coral Derivatives", "Ivory and Items Containing Ivory", "Live Animals and Birds", "Live Plants", "Parts or Derivatives of Animals/Birds", "Parts or Derivatives of Plants", "Timber or Wood Products", "Preparations Of Oriental Medicine Which Include Parts & Derivatives Of Endangered Species", "Butterflies"))
+  select(c("Year", "Quarter", "Total Number of Seizures", "Caviar and Caviar extract", "Live Coral and Coral Derivatives", "Ivory and Items Containing Ivory", "Live Animals and Birds", "Live Plants", "Parts or Derivatives of Animals/Birds", "Parts or Derivatives of Plants", "Timber or Wood Products", "Preparations Of Oriental Medicine Which Include Parts and Derivatives Of Endangered Species", "Butterflies"))
 
 # remove rows containing only NAs
 
@@ -49,19 +49,19 @@ renamed_seizures_main_data[renamed_seizures_main_data == ":"] <- 0
 # calculate total seizures for each import type for each year
 
 seizures_main_data_totals <-  renamed_seizures_main_data %>% 
-  select(c("Year", "Total Number of Seizures", "Caviar & Caviar extract", "Live Coral & Coral Derivatives", "Ivory and Items Containing Ivory", "Live Animals and Birds", "Live Plants", "Parts or Derivatives of Animals/Birds", "Parts or Derivatives of Plants", "Timber or Wood Products", "Preparations Of Oriental Medicine Which Include Parts & Derivatives Of Endangered Species", "Butterflies"))
+  select(c("Year", "Total Number of Seizures", "Caviar and Caviar extract", "Live Coral and Coral Derivatives", "Ivory and Items Containing Ivory", "Live Animals and Birds", "Live Plants", "Parts or Derivatives of Animals/Birds", "Parts or Derivatives of Plants", "Timber or Wood Products", "Preparations Of Oriental Medicine Which Include Parts and Derivatives Of Endangered Species", "Butterflies"))
 
 seizures_main_data_totals$`Year` <- as.numeric(seizures_main_data_totals$`Year`)
 seizures_main_data_totals$`Total Number of Seizures` <- as.numeric(seizures_main_data_totals$`Total Number of Seizures`)
-seizures_main_data_totals$`Caviar & Caviar extract` <- as.numeric(seizures_main_data_totals$`Caviar & Caviar extract`)
-seizures_main_data_totals$`Live Coral & Coral Derivatives` <- as.numeric(seizures_main_data_totals$`Live Coral & Coral Derivatives`)
+seizures_main_data_totals$`Caviar and Caviar extract` <- as.numeric(seizures_main_data_totals$`Caviar and Caviar extract`)
+seizures_main_data_totals$`Live Coral and Coral Derivatives` <- as.numeric(seizures_main_data_totals$`Live Coral and Coral Derivatives`)
 seizures_main_data_totals$`Ivory and Items Containing Ivory` <- as.numeric(seizures_main_data_totals$`Ivory and Items Containing Ivory`)
 seizures_main_data_totals$`Live Animals and Birds` <- as.numeric(seizures_main_data_totals$`Live Animals and Birds`)
 seizures_main_data_totals$`Live Plants` <- as.numeric(seizures_main_data_totals$`Live Plants`)
 seizures_main_data_totals$`Parts or Derivatives of Animals/Birds` <- as.numeric(seizures_main_data_totals$`Parts or Derivatives of Animals/Birds`)
 seizures_main_data_totals$`Parts or Derivatives of Plants` <- as.numeric(seizures_main_data_totals$`Parts or Derivatives of Plants`)
 seizures_main_data_totals$`Timber or Wood Products` <- as.numeric(seizures_main_data_totals$`Timber or Wood Products`)
-seizures_main_data_totals$`Preparations Of Oriental Medicine Which Include Parts & Derivatives Of Endangered Species` <- as.numeric(seizures_main_data_totals$`Preparations Of Oriental Medicine Which Include Parts & Derivatives Of Endangered Species`)
+seizures_main_data_totals$`Preparations Of Oriental Medicine Which Include Parts and Derivatives Of Endangered Species` <- as.numeric(seizures_main_data_totals$`Preparations Of Oriental Medicine Which Include Parts and Derivatives Of Endangered Species`)
 seizures_main_data_totals$`Butterflies` <- as.numeric(seizures_main_data_totals$`Butterflies`)
 
 seizures_main_data_totals <- aggregate(x = seizures_main_data_totals[ , colnames(seizures_main_data_totals) != "Year"],

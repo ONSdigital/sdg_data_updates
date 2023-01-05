3430c684812441e38ba3f083af259566da237771
Nairn <Michael.Nairn@ons.gov.uk>
2023-01-05 12:27:45 +0000

    R md file part of compile tables script included

--- 5-a-1/compile_tables.R
+++ 5-a-1/compile_tables.R
@@ -51,8 +51,10 @@ qa_filename <- paste0(date, "_5-a-1.html")
 
 write.csv(csv_output, paste0(output_folder, "/", csv_filename), row.names = FALSE)
 
+
 # # If you have a QA document written in Rmarkdown this is how you can run it and save it
-# rmarkdown::render('type_1_checks.Rmd', output_file = paste0(output_folder, "/", qa_filename))
+rmarkdown::render('5-a-1_checks.Rmd', output_file = paste0(output_folder, "/", qa_filename))
+
 
 message(paste0("The csv and QA file have been created and saved in '", paste0(getwd(), "/", output_folder, "'"),
                " as ", csv_filename, "and ", qa_filename, "'\n\n"))

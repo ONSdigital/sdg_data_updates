2a671b34fc536785e819c855b1e85820a79a3b94
Michael Nairn <96240844+MichaelGNairn@users.noreply.github.com>
2023-01-09 14:39:18 +0000

    Add files via upload
    Signed-off-by: Michael Nairn <96240844+MichaelGNairn@users.noreply.github.com>

---
+++ 5-2-1/config.R
@@ -0,0 +1,23 @@
+# configurations for 5-2-1 update
+# Michael Nairn
+#28/11/2022
+
+filename <- "domesticabuseprevalenceandvictimcharacteristics2022.xlsx"
+
+tabname1 <- "Table 1" 
+# Prevalence of domestic abuse among adults aged 16 years and over, by type of abuse and sex, 
+
+tabname3 <- "Table 3" 
+# Prevalence of domestic abuse in the last year among adults aged 16 to 59, by type of abuse and sex, 
+
+tabname6 <- "Table 6"
+# Prevalence of domestic abuse in the last year among adults aged 16 years and over, by personal characteristics and sex, 
+
+tabname7 <- "Table 7"
+# Prevalence of domestic abuse in the last year among adults aged 16 years and over, by household and area characteristics and sex, 
+
+first_header_row <- 7
+
+input_folder  <- "Input"
+output_folder <- "Output"
+

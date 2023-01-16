3e8a2731cf1b46ca80410508babd8aae90a90175
Campbell <Ali.Campbell@ons.gov.uk>
2023-01-16 10:02:05 +0000

    Checking git is committing changes correctly

--- 5-6-1/compile_tables.R
+++ 5-6-1/compile_tables.R
@@ -21,27 +21,16 @@ library('dplyr')
 
 library('readxl')
 
+# Oi oi oi !
 
 source("config.R") # pulls in all the configurations. Un-comment out code below for real update
-if (test_run == TRUE) {
-  source("example_config.R")
-} else if (test_run == FALSE) {
- source("config.R")
-} else {
- stop("test_run must be either TRUE or FALSE")
-}
-
-
-csv <- NULL
-
-for (i in 1:length(years)) {
-  age_source_data <- paste0(filename, tabname_age, years[i])
-  la_source_data <- paste0(filename, tabname_la, years[i])
-  
-  
-  csv <- bind_rows(csv, csv_output)
-}
-
+#if (test_run == TRUE) {
+#  source("example_config.R")
+#} else if (test_run == FALSE) {
+# source("config.R")
+#} else {
+# stop("test_run must be either TRUE or FALSE")
+#}
 
 source("update_5-6-1.R") # does the donkey-work of making the csv - 
                           # for real update this might be called e.g. 'update_1-2-1.R' 

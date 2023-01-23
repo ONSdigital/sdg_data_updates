5d66c8027b82e42da6eed69f6293c864adfcc7bb
Nairn <Michael.Nairn@ons.gov.uk>
2023-01-23 13:59:06 +0000

    Correction of comparison datatable feature of checks table

--- update_indicator_main.R
+++ update_indicator_main.R
@@ -12,7 +12,7 @@
 setwd("D:/Coding_repos/sdg_data_updates")
 install.packages("SDGupdater", repos = NULL, type="source", force = TRUE)
 
-# rm(list = ls())
+rm(list = ls())
 
 
 indicator <- "3-4-1" # name of folder for indicator

087dd46e200c5e012e7842dc2206f4a5a8570e0a
Nairn <Michael.Nairn@ons.gov.uk>
2023-02-17 13:52:08 +0000

    example_config file

---
+++ 3-9-2/example_config.R
@@ -0,0 +1,18 @@
+# configurations for type 1 data
+
+filename <- "type_1_data.xlsx"
+# # If you are using csv data make sure numbers are formatted as numbers in Excel 
+# # and DO NOT include the thousands separator
+# # tabname will not be needed if you are using csv data
+# filename <- "type_1_metadata_included.csv"
+# filename <- "type_1_no_metadata.csv"
+
+# tabname <- "no_metadata"
+tabname <- "metadata_included"
+
+# header_row <- 1
+header_row <- 3
+
+input_folder  <- "Example_input"
+output_folder <- "Example_output"
+

e8ef1ef3c926349029ac5aba58418885b2981e5f
Uzzell <Katie.Uzzell@ons.gov.uk>
2023-02-22 12:10:23 +0000

    produce QA file

--- 3-a-1/update_3-a-1.R
+++ 3-a-1/update_3-a-1.R
@@ -17,12 +17,28 @@ combined_data <- list.files(path = "./CSVs",
 
 csv_output <- combined_data %>% 
   select("Year", "Series", "Sex","Age group", "Country", "Region of England", "Socio-economic group", "Ethnicity", "Country of birth", "Units", 
-         "Unit multiplier", "Observation status", "Value")
+         "Unit multiplier", "Observation status", "Value") %>% 
+  rename("Age" = "Age group",
+       "Region" = "Region of England",
+       "Socio-economic classification" = "Socio-economic group")
+
+csv_output <- csv_output %>% replace_na(list(Country = "England"))
 
 csv_output <- sapply(csv_output, as.character)
 csv_output[is.na(csv_output)] <- ""
 csv_output[csv_output == "Persons"] <- ""
 csv_output[csv_output == "Women"] <- "Female"
 csv_output[csv_output == "Men"] <- "Male"
+csv_output[csv_output == "18-24"] <- "18 to 24"
+csv_output[csv_output == "25-34"] <- "25 to 34"
+csv_output[csv_output == "35-44"] <- "35 to 44"
+csv_output[csv_output == "45-54"] <- "45 to 54"
+csv_output[csv_output == "55-64"] <- "55 to 64"
+csv_output[csv_output == "65+"] <- "65 and over"
+csv_output[csv_output == "All 18+"] <- ""
+csv_output[csv_output == "East of England"] <- "East"
+csv_output[csv_output == "United Kingdom"] <- ""
+
+  
 
 

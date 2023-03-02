1b190afb7e15fbbc3e78d009f76758cb19fabbde
Singh <Abhishek.Singh@ons.gov.uk>
2023-03-02 17:43:57 +0000

    Check_file completed with graph

--- 8-6-1/example_config.R
+++ 8-6-1/example_config.R
@@ -9,31 +9,42 @@ header_row <- 0
 
 
 # Important Steps! Need to provide these values (table start and end row)
+# Table are in the given order followed below
 # Do not include the header row as start row
+
+
+
+
+# Seasonally Adjusted
 
 # Not in Education, Employment or Training ('NEET') by age: People (seasonally adjusted)
 table_people_sa_START_row <- 21
-table_people_sa_END_row <- 102
+table_people_sa_END_row <- 105
 
 # Not in Education, Employment or Training ('NEET') by age: Men (seasonally adjusted)
-table_men_sa_START_row <- 111
-table_men_sa_END_row <- 192
+table_men_sa_START_row <- 118
+table_men_sa_END_row <- 202
 
 # Not in Education, Employment or Training ('NEET') by age: Women (seasonally adjusted)
-table_woman_sa_START_row <- 203
-table_woman_sa_END_row <- 284
-  
+table_woman_sa_START_row <- 216
+table_woman_sa_END_row <- 300
+
+
+
+# Not Seasonally Adjusted
+
 # Not in Education, Employment or Training ('NEET') by age: People (not seasonally adjusted)  
-table_people_nsa_START_row <- 294
-table_people_nsa_END_row <- 375
+table_people_nsa_START_row <- 310
+table_people_nsa_END_row <- 394
 
 # Not in Education, Employment or Training ('NEET') by age: Men (not seasonally adjusted)
-table_men_nsa_START_row <- 385
-table_men_nsa_END_row <- 466
+table_men_nsa_START_row <- 404
+table_men_nsa_END_row <- 488
 
-# Not in Education, Employment or Training ('NEET') by age: Men (not seasonally adjusted)
-table_woman_nsa_START_row <- 476
-table_woman_nsa_END_row <- 557
+# Not in Education, Employment or Training ('NEET') by age: Women (not seasonally adjusted)
+table_woman_nsa_START_row <- 500
+table_woman_nsa_END_row <- 584
+
 
 
 

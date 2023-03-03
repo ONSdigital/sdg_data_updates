ad9216f03d4a455f43da6a648a30b0408e64beb2
Singh <Abhishek.Singh@ons.gov.uk>
2023-03-03 12:15:46 +0000

    Check file completed

--- 8-6-1/example_config.R
+++ 8-6-1/example_config.R
@@ -19,31 +19,33 @@ header_row <- 0
 
 # Not in Education, Employment or Training ('NEET') by age: People (seasonally adjusted)
 table_people_sa_START_row <- 21
-table_people_sa_END_row <- 105
+table_people_sa_END_row <- 102
 
 # Not in Education, Employment or Training ('NEET') by age: Men (seasonally adjusted)
-table_men_sa_START_row <- 118
-table_men_sa_END_row <- 202
+table_men_sa_START_row <- 111
+table_men_sa_END_row <- 192
 
 # Not in Education, Employment or Training ('NEET') by age: Women (seasonally adjusted)
-table_woman_sa_START_row <- 216
-table_woman_sa_END_row <- 300
+table_woman_sa_START_row <- 203
+table_woman_sa_END_row <- 284
+
 
 
 
 # Not Seasonally Adjusted
 
 # Not in Education, Employment or Training ('NEET') by age: People (not seasonally adjusted)  
-table_people_nsa_START_row <- 310
-table_people_nsa_END_row <- 394
+table_people_nsa_START_row <- 294
+table_people_nsa_END_row <- 375
 
 # Not in Education, Employment or Training ('NEET') by age: Men (not seasonally adjusted)
-table_men_nsa_START_row <- 404
-table_men_nsa_END_row <- 488
+table_men_nsa_START_row <- 385
+table_men_nsa_END_row <- 466
 
 # Not in Education, Employment or Training ('NEET') by age: Women (not seasonally adjusted)
-table_woman_nsa_START_row <- 500
-table_woman_nsa_END_row <- 584
+table_woman_nsa_START_row <- 476
+table_woman_nsa_END_row <- 557
+
 
 
 

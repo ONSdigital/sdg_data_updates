11a7c9684dfc8966fb74d4190c1a15bbf5d675a1
Atanaska <atanaska.nikolova@ons.gov.uk>
2023-03-24 12:15:17 +0000

    Adding CRS codes for 7.a.1

--- update_indicator_main.R
+++ update_indicator_main.R
@@ -11,7 +11,7 @@
 # Because SDGupdater is a local package we install it slightly differently:
 
 #setwd("D:/Coding_repos/sdg_data_updates")
-install.packages("SDGupdater", repos = NULL, type="source", force = TRUE)
+#install.packages("SDGupdater", repos = NULL, type="source", force = TRUE)
 
 rm(list = ls())
 

9baa7098fb2f16650c39f2ac7998ca0a3fc576b3
Atanaska <atanaska.nikolova@ons.gov.uk>
2023-04-13 15:50:43 +0100

    Edits 17-19-1 to conform to UN metadata

--- ODA/17-19-1.R
+++ ODA/17-19-1.R
@@ -16,16 +16,50 @@ gbp_data <- bind_rows(data_17191,
            "Unspecified", country_income_classification)
   )
 
-constant_usd_data <-  gbp_to_constant_usd(rates_filepath, deflators_filepath, 
-                                          gbp_data)
+#constant_usd_data <-  gbp_to_constant_usd(rates_filepath, deflators_filepath, 
+#                                          gbp_data)
+read_exchange_rates <- function(rates_filepath) {
+  tidyxl::xlsx_cells(rates_filepath)
+}
 
+tidy_data <- function(dat, heading_row) {
+  dat %>% 
+    dplyr::filter(row >= heading_row) %>% 
+    unpivotr::behead("left", country) %>% 
+    unpivotr::behead("up", year) %>% 
+    dplyr::filter(!is.na(numeric)) %>% 
+    dplyr::select(year, country, numeric)
+}
+
+apply_exchange_rate <- function(gbp_data, uk_exchange_data) {
+  names(gbp_data) <- tolower(names(gbp_data))
+  names(uk_exchange_data) <- tolower(names(uk_exchange_data))
+  
+  gbp_data %>% 
+    dplyr::left_join(uk_exchange_data, by = "year" ) %>% 
+    dplyr::mutate(value = value/exchange_rate,
+                  units = "USD ($ thousands)") %>% 
+    dplyr::select(-exchange_rate)
+}
+
+gbp_to_usd <- function(exchange_rates, gbp_data) {
+  
+  heading_row <- which(!is.na(exchange_rates$numeric))[1]
+  exchange_data <- tidy_data(exchange_rates, heading_row)
+  uk_exchange_data <- get_uk_values(exchange_data, exchange_rate)
+  usd_data <- apply_exchange_rate(gbp_data, uk_exchange_data)
+  
+  return(usd_data)
+}
+exchange_rates <- read_exchange_rates(rates_filepath)
+usd_data <- gbp_to_usd(exchange_rates, gbp_data)
 names(gbp_data) <- str_to_sentence(names(gbp_data))
-names(constant_usd_data) <- str_to_sentence(names(constant_usd_data))
-
+#names(constant_usd_data) <- str_to_sentence(names(constant_usd_data))
+names(usd_data) <- str_to_sentence(names(usd_data))
 csv <- gbp_data %>% 
-  bind_rows(constant_usd_data) %>% 
+  bind_rows(usd_data) %>% 
   mutate(Series = "Value of all resources made available to strengthen statistical capacity in developing countries",
-         `Observation status` = "Definition differs") %>% 
+         `Observation status` = "Normal value") %>% 
   select(Year, Series, Country_income_classification,
          `Observation status`, Units, Value) %>% 
   arrange(Year, Country_income_classification) %>% 
@@ -33,6 +67,6 @@ csv <- gbp_data %>%
   rename(`Country income classification` = Country_income_classification)
 
 rm(data_17191, headline,
-   gbp_data, constant_usd_data)
+   gbp_data, usd_data)
 
 scripts_run <- c(scripts_run, "17-19-1")
--- update_indicator_main.R
+++ update_indicator_main.R
@@ -14,3 +14,7 @@ rm(list = ls())
 test_run <- FALSE # for real updates use FALSE
 
 indicator <- "ODA" # name of folder for indicator
+
+setwd(indicator)
+
+source("compile_tables.R")

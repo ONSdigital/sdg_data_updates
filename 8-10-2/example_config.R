f7b370c5e7060f5ed3297e85d2372b9001ac9a4b
Uzzell <Katie.Uzzell@ons.gov.uk>
2023-04-25 16:11:36 +0100

    setting up 8-10-2 ready for automation

---
+++ 8-10-2/example_config.R
@@ -0,0 +1,12 @@
+# configurations for 8-10-2
+
+filename <- "Ch7_Savings_Investments.xlsx"
+
+# tabname <- "no_metadata"
+tabname <- "7_7"
+
+# header_row <- 1
+header_row <- 8
+
+input_folder  <- "Example_Input"
+output_folder <- "Example_Output"

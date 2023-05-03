cc0d4dc8ee55cfde267cea7fd6628e7185f9ad87
Uzzell <Katie.Uzzell@ons.gov.uk>
2023-05-03 07:39:17 +0100

    setting up and working on 8-1-1 automation

---
+++ 8-1-1/example_config.R
@@ -0,0 +1,21 @@
+# configurations for type 1 data
+
+filename <- "ukea.xlsx"
+# # If you are using csv data make sure numbers are formatted as numbers in Excel 
+# # and DO NOT include the thousands separator
+# # tabname will not be needed if you are using csv data
+# filename <- "type_1_metadata_included.csv"
+# filename <- "type_1_no_metadata.csv"
+
+# tabname <- "no_metadata"
+tabname <- "data"
+
+# header_row <- 1
+header_row <- 2
+
+selected_years <- c(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
+                    2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
+                    2020, 2021, 2022)
+
+input_folder  <- "Example_Input"
+output_folder <- "Example_Output"

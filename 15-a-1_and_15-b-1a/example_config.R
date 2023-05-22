3aa35320b26734ac8c35c2d9bfafd9cef8f00410
Atanaska <atanaska.nikolova@ons.gov.uk>
2023-05-22 07:29:35 +0100

    Deleting obsolete indicator folder

--- 15-a-1_and_15-b-1a/example_config.R
+++
@@ -1,8 +0,0 @@
-# configurations for 15.a.1/15.b.1
-# Change the names of the input csv files accordingly
-# input_folder name should be changed to whatever folder name you place the csv files in (e.g. "Input")
-
-filename_1 <- "dataunderSID-Final2020.csv"
-filename_2 <- "data-underlying-sid-2017.csv"
-
-input_folder  <- "Example_Input"

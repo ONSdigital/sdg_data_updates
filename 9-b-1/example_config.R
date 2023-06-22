36e3f68ab586170c05b73ac020d1b828e1512406
Uzzell <Katie.Uzzell@ons.gov.uk>
2023-06-22 06:46:24 +0100

    review changes requested to config

--- 9-b-1/example_config.R
+++ 9-b-1/example_config.R
@@ -1,13 +1,7 @@
-# configurations for type 1 data
+# configurations for 9-b-1
 
 filename <- "gdpolowlevelaggregates2022q4.xlsx"
-# # If you are using csv data make sure numbers are formatted as numbers in Excel 
-# # and DO NOT include the thousands separator
-# # tabname will not be needed if you are using csv data
-# filename <- "type_1_metadata_included.csv"
-# filename <- "type_1_no_metadata.csv"
 
-# tabname <- "no_metadata"
 tabname <- "2a"
 
 selected_years <- c(1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 

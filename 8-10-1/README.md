e9e8e1a305c1b5df6f9eaef6cee5e86e6415a7c9
Uzzell <Katie.Uzzell@ons.gov.uk>
2023-07-04 10:38:52 +0100

    set up

---
+++ 8-10-1/README.md
@@ -0,0 +1,25 @@
+Authors(s): Katie Uzzell
+Date: 04/07/2023
+
+### 8-10-1 automation
+
+Creates csv data for 8-10-1 (Part (a) only - Number of commercial bank branches per 100,000 adults).
+
+Fairly simple automation that involves calculating the number of banks and building societies per 100,000 people, disaggregated by country, region, and local authority and formatting the data into csv format ready for the platform. 
+
+Runtime is less than XXXXXXX seconds.
+
+Output includes the data in csv format, and an html QA report. Users should still look at the source data and check footnotes and information tabs for anything that needs to be manually changed in the csv output (e.g., if any figures are estimated) or caveats that need to be included in the metadata. 
+
+### 8-10-1 update instructions
+
+1) Download the latest data from: 
+2) Save the files as an xlsx file in the 'Input' folder in 8-10-1 (if this doesn't exist, make it inside the 8-10-1 folder).  
+3) In RStudio, go to File > Open Project, and open sdg_data_updates.Rproj. It may take a few minutes to load, please be patient. Update_indicator_main.R should open. If it doesn't go to File > Open File, and open it. 
+4) If it exists, open the `config.R` file in 8-10-1 (you can do this in the 'Files' panel in RStudio (usually a tab in the bottom right panel). If not, save example_config.R as `config.R` in 8-10-1.
+5) Check the configurations (e.g. filename and tab names) are correct, and if not correct them and save. For example, if you have had to save example_config.R as config.R, make sure 'Example_Input' and 'Example_Output' are changed to 'Input' and 'Output' and the file name is correct. **Make sure the selected years and latest year configs are correct - if you are updating this indicator, it is likely you will have to add the latest year(s) to the list**  
+6) Open `update_indicator_main.R` (from `sdg_data_updates.Rproj`) and set indicator as 8-10-1. Make sure test_run is set to FALSE (so that it sources the correct config file). 
+7) Click 'Source' button to run the script (top right corner of the script panel).  
+8) Outputs will be saved in the Output folder in 8-10-1 (which the script will create if it doesn't already exist).  
+9) An html file will also be created in the Outputs folder. This contains some basic checks and also shows new data plotted over old data (i.e., what is on the live site), which should show up any major issues. **Please check this file before copying to the csv.**
+
---
+++ 8-10-1/update_8-10-1.R
@@ -0,0 +1,93 @@
+# author: Katie Uzzell
+# date: 04/07/2023
+
+# Code to automate data update for indicator 8-10-1 
+# (Part (a) only - Number of commercial bank branches per 100,000 adults).
+
+# read in data 
+
+banks_by_country_source <- get_type1_data(banks_bs_header_row, banks_country, tabname)
+banks_by_region_source <- get_type1_data(banks_bs_header_row, banks_region, tabname)
+banks_by_la_source <- get_type1_data(banks_bs_header_row, banks_la, tabname)
+bs_by_country_source <- get_type1_data(banks_bs_header_row, bs_country, tabname)
+bs_by_region_source <- get_type1_data(banks_bs_header_row, bs_region, tabname)
+bs_by_la_source <- get_type1_data(banks_bs_header_row, bs_la, tabname)
+pop_ests_by_country_source <- get_type1_data(pop_ests_header_row, pop_ests_country, tabname)
+pop_ests_by_region_source <- get_type1_data(pop_ests_header_row, pop_ests_region, tabname)
+pop_ests_by_la_source <- get_type1_data(pop_ests_header_row, pop_ests_la, tabname)
+
+# remove cells above column names
+
+banks_by_country_main <- extract_data(banks_by_country_source, banks_bs_header_row)
+banks_by_region_main <- extract_data(banks_by_region_source, banks_bs_header_row)
+banks_by_la_main <- extract_data(banks_by_la_source, banks_bs_header_row)
+bs_by_country_main <- extract_data(bs_by_country_source, banks_bs_header_row)
+bs_by_region_main <- extract_data(bs_by_region_source, banks_bs_header_row)
+bs_by_la_main <- extract_data(bs_by_la_source, banks_bs_header_row)
+pop_ests_by_country_main <- extract_data(pop_ests_by_country_source, pop_ests_header_row)
+pop_ests_by_region_main <- extract_data(pop_ests_by_region_source, pop_ests_header_row)
+pop_ests_by_la_main <- extract_data(pop_ests_by_la_source, pop_ests_header_row)
+
+banks_by_la_main <- banks_by_la_main %>%  
+  rename("Local Authority" = "local authority: district / unitary (as of April 2021)")
+bs_by_la_main <- bs_by_la_main %>%  
+  rename("Local Authority" = "local authority: district / unitary (as of April 2021)")
+pop_ests_by_la_main <- pop_ests_by_la_main %>%  
+  rename("Local Authority" = "local authority: district / unitary (as of April 2021)")
+
+banks_by_la_main$`2010` <- as.numeric(as.character(banks_by_la_main$`2010`))
+bs_by_la_main$`2010` <- as.numeric(as.character(bs_by_la_main$`2010`))
+pop_ests_by_la_main$`2010` <- as.numeric(as.character(pop_ests_by_la_main$`2010`))
+
+banks_by_la_main <- banks_by_la_main %>% 
+  pivot_longer(-c("Local Authority"), names_to = "Year", values_to = "Banks")
+bs_by_la_main <- bs_by_la_main %>% 
+  pivot_longer(-c("Local Authority"), names_to = "Year", values_to = "Building Societies")
+pop_ests_by_la_main <- pop_ests_by_la_main %>% 
+  pivot_longer(-c("Local Authority"), names_to = "Year", values_to = "Population Estimate")
+
+la_data <- full_join(banks_by_la_main, bs_by_la_main, by = c("Local Authority", "Year"))
+
+la_data <- full_join(la_data, pop_ests_by_la_main, by = c("Local Authority", "Year"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# select wanted columns and rows
+
+gdp_main_data <- gdp_main_data %>% 
+  select(c("CDID","N3Y6"))
+
+gdp_main_data <- gdp_main_data[gdp_main_data$CDID >= 2000 & gdp_main_data$CDID <= latest_year, ]
+
+gdp_main_data <- gdp_main_data[!grepl("Q", gdp_main_data$CDID),]
+
+# format
+
+gdp_main_data <- gdp_main_data %>% 
+  rename("Year" = CDID, "Value" = N3Y6)
+
+csv_formatted <- gdp_main_data %>% 
+         mutate("Series" = "Annual growth rate of real GDP per capita",
+           "Unit measure" = "Percentage (%)",
+            "Unit multiplier" =  "Units",
+            "Observation status" = "Normal value")
+
+csv_output <- csv_formatted %>%            
+select("Year", "Series", "Unit measure", "Unit multiplier", "Observation status", "Value")
+
+
+
+
+
+

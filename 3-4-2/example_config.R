3e711ce9b164ac0fb084a50a5070919f35a88462
Nairn <Michael.Nairn@ons.gov.uk>
2023-07-28 13:22:56 +0100

    Setting up 3-4-2 automation

---
+++ 3-4-2/example_config.R
@@ -0,0 +1,17 @@
+# Michael Nairn 12/04/2023
+
+# configurations for Indicator update 3-5-1
+
+treatment_England <- "ViewIt Export Data_England.csv"
+treatment_LA <- "ViewIt Export Data_LAs.csv"
+
+unmet_opiates <- "unmet_opiates.csv"
+unmet_alcohol <- "unmet_alcohol.csv"
+
+alcohol_prevalence <- "Estimates_of_Alcohol_Dependent_Adults_in_England_for_R.csv"
+
+
+input_folder  <- "Example_Input"
+output_folder <- "Example_Output"
+
+

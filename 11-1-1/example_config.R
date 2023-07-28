22bc89c30741b75cb14e7c673e083aa9f6dd742b
Michael Nairn <96240844+MichaelGNairn@users.noreply.github.com>
2023-07-28 09:23:47 +0100

    Update example_config.R
    Signed-off-by: Michael Nairn <96240844+MichaelGNairn@users.noreply.github.com>

--- 11-1-1/example_config.R
+++ 11-1-1/example_config.R
@@ -6,9 +6,8 @@ households_filename <- "Example_households.xlsx"
 areas_header_row <- 5
 households_header_row <- 5
 
-tabnames <- c("2019", 
-              "2020",
-             "2021")
+tabnames <- c("2008", "2009", "2010", "2011", "2012", "2013", "2014",
+              "2015", "2016", "2017", "2018", "2019", "2020", "2021")
 
 input_folder  <- "Example_input"
 output_folder  <- "Example_output"

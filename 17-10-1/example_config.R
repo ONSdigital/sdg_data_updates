fcac1c527915d4efad18f191689d31fe8948706c
Gummer <Katie.Gummer@ons.gov.uk>
2023-08-09 10:23:25 +0100

    completed automation script

--- 17-10-1/example_config.R
+++ 17-10-1/example_config.R
@@ -1,30 +1,8 @@
 # configurations for 17-10-1 example update
 
-Jan_data <- "BDSPref2201.csv"
-Feb_data <- "BDSPref2202.csv"
-Mar_data <- "BDSPref2203.csv"
-Apr_data <- "BDSPref2204.csv"
-May_data <- "BDSPref2205.csv"
-Jun_data <- "BDSPref2206.csv"
-Jul_data <- "BDSPref2207.csv"
-Aug_data <- "BDSPref2208.csv"
-Sep_data <- "BDSPref2209.csv"
-Oct_data <- "BDSPref2210.csv"
-Nov_data <- "BDSPref2211.csv"
-Dec_data <- "BDSPref2212.csv"
+source_data <- "17-10-1_SDG_database.xlsx"
 
-Jan_tabname <- "BDSPref2201"
-Feb_tabname <- "BDSPref2202"
-Mar_tabname <- "BDSPref2203"
-Apr_tabname <- "BDSPref2204"
-May_tabname <- "BDSPref2205"
-Jun_tabname <- "BDSPref2206"
-Jul_tabname <- "BDSPref2207"
-Aug_tabname <- "BDSPref2208"
-Sep_tabname <- "BDSPref2209"
-Oct_tabname <- "BDSPref2210"
-Nov_tabname <- "BDSPref2211"
-Dec_tabname <- "BDSPref2212"
+tabname <- "Goal17"
 
 
 header_row <- 1
--- 17-10-1/update_17-10-1.R
+++ 17-10-1/update_17-10-1.R
@@ -5,70 +5,44 @@
 
 # read in data 
 
-Jan_data <- get_type1_data(header_row, Jan_data, Jan_tabname)
-Feb_data <- get_type1_data(header_row, Feb_data, Feb_tabname)
-Mar_data <- get_type1_data(header_row, Mar_data, Mar_tabname)
-Apr_data <- get_type1_data(header_row, Apr_data, Apr_tabname)
-May_data <- get_type1_data(header_row, May_data, May_tabname)
-Jun_data <- get_type1_data(header_row, Jun_data, Jun_tabname)
-Jul_data <- get_type1_data(header_row, Jul_data, Jul_tabname)
-Aug_data <- get_type1_data(header_row, Aug_data, Aug_tabname)
-Sep_data <- get_type1_data(header_row, Sep_data, Sep_tabname)
-Oct_data <- get_type1_data(header_row, Oct_data, Oct_tabname)
-Nov_data <- get_type1_data(header_row, Nov_data, Nov_tabname)
-Dec_data <- get_type1_data(header_row, Dec_data, Dec_tabname)
+main_data <- get_type1_data(header_row, source_data, tabname)
 
-#joining datasets together 
+#select the necessary columns 
+trade_data <- main_data %>% 
+  select("TimePeriod", "SeriesCode", "Type.of.product", "Value")
 
-all_data <- full_join(Jan_data, Feb_data, by = c("Date", "Country"))
+#renaming column 
+trade_data <- trade_data %>%
+  rename("Year" = "TimePeriod", "Series" = "SeriesCode", "Type of product" = "Type.of.product")
+
+#correcting data
+trade_data$Series[trade_data$Series == 'TM_TAX_WMFN'] <- 'Most favoured nation status'
+
+trade_data$Series[trade_data$Series == 'TM_TAX_WMPS'] <- 'Preferential status'
+
+trade_data$`Type of product`[trade_data$`Type of product` == 'ARM'] <- 'Arms'
+trade_data$`Type of product`[trade_data$`Type of product` == 'AGR'] <- 'Agriculture'
+trade_data$`Type of product`[trade_data$`Type of product` == 'ALP'] <- 'Total or no breakdown'
+trade_data$`Type of product`[trade_data$`Type of product` == 'TEX'] <- 'Textiles'
+trade_data$`Type of product`[trade_data$`Type of product` == 'OIL'] <- 'Oil'
+trade_data$`Type of product`[trade_data$`Type of product` == 'IND'] <- 'Industrial products'
+trade_data$`Type of product`[trade_data$`Type of product` == 'CLO'] <- 'Clothing'
 
 
-# remove cells above column names
-
-energy_main_data <- extract_data(energy_source_data, header_row)
-
-# rename column 3
-
-colnames(energy_main_data) [1] <- "Sector code"
-colnames(energy_main_data) [2] <- "Section code"
-colnames(energy_main_data) [3] <- "Industry sector"
-
-# remove unwanted columns
-
-energy_main_data <- within(energy_main_data, rm("Sector code","Section code"))
-
-# select only needed rows 
-
-selected_rows <- c(1:22)
-
-energy_main_data <- energy_main_data %>%
-  slice(selected_rows)
-
-energy_main_data <- energy_main_data %>% 
-  na.omit()
-
-# format
-
-energy_csv <- energy_main_data %>% 
-  pivot_longer(-c("Industry sector"), names_to = "Year", values_to = "Value")
-
-energy_csv$`Industry sector` <- sub("Total", "", energy_csv$`Industry sector`)
 
 # Format csv 
 
-csv_formatted <- energy_csv %>% 
-         mutate("Series" = "Energy intensity level of primary energy",
-           "Unit measure" = "Terajoules per million pounds (TJ/£ million)",
+csv_formatted <- trade_data %>% 
+         mutate("Units" = "Percentage(%)",
             "Unit multiplier" =  "Units",
             "Observation status" = "Normal value")
 
-# The below line may or may not be necessary in future releases
-# csv_formatted$Year <- gsub("2020 2", "2020", csv_formatted$Year)
 
-csv_formatted <- csv_formatted %>%            
-select("Year", "Series", "Industry sector", "Unit measure", "Unit multiplier", "Observation status", "Value")
 
-csv_output <- csv_formatted[order(csv_formatted$`Industry sector`), ]
+csv_output <- csv_formatted %>%            
+select("Year", "Series", "Type of product", "Units", "Unit multiplier", "Observation status", "Value")
+
+
 
 
 

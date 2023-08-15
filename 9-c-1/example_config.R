54d4fffdc4e329c4204ff4f9d700ae41f7bf7686
Gummer <Katie.Gummer@ons.gov.uk>
2023-08-15 16:04:40 +0100

    automation completed

---
+++ 9-c-1/example_config.R
@@ -0,0 +1,11 @@
+# configurations for 9-c-1 example update
+
+filename <- "9-c-1_data_from_SDG_database.xlsx"
+
+tabname <- "Goal9"
+
+
+header_row <- 1
+
+input_folder  <- "Example_Input"
+output_folder <- "Example_Output"
--- update_indicator_main.R
+++ update_indicator_main.R
@@ -14,9 +14,9 @@ install.packages("SDGupdater", repos = NULL, type="source", force = TRUE)
 
 rm(list = ls())
 
-test_run <- FALSE # for real updates use FALSE
+test_run <- TRUE # for real updates use FALSE
 
-indicator <- "3-4-2" # name of folder for indicator
+indicator <- "9-C-1" # name of folder for indicator
 
 setwd(indicator)
 

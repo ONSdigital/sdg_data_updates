e94250eb5f4ea73849ba3b2bc09580ac04234197
Nairn <Michael.Nairn@ons.gov.uk>
2023-08-15 09:03:00 +0100

    3-b-1 compile tables and config

---
+++ 3-b-1/example_config.R
@@ -0,0 +1,28 @@
+# Michael Nairn 15/08/2023
+
+# configurations for Indicator update 3-b-1
+
+
+input_folder  <- "Example_Input"
+output_folder <- "Example_Output"
+
+
+filename <- paste0(input_folder, "/", "mainaccessible.xlsx")
+
+#same format, can combine
+tabname_EandW <- "Table_1"
+tabname_England <- "Table_2"
+tabname_Wales <- "Table_3"
+
+tabname_region <- "Table_4" # years go across here, instead of down.
+
+#same format, can combine
+tabname_age_EandW <- "Table_5"
+tabname_age_England <- "Table_6"
+tabname_age_Wales <- "Table_7"
+
+
+header_row <- 4
+
+
+

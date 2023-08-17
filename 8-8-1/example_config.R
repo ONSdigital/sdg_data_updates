2dfa740d1368472c7f31ccdcf9d1abef0ba04732
Uzzell <Katie.Uzzell@ons.gov.uk>
2023-08-17 07:36:20 +0100

    setting up files for automation

---
+++ 8-8-1/example_config.R
@@ -0,0 +1,22 @@
+# configurations for 8-8-1 (Rates of fatal and non-fatal occupational injuries 
+# (excluding injuries arising from road traffic accidents)).
+
+fatal_inj_headline <- "ridhist.xlsx"
+fatal_inj_region <- "ridreg.xlsx"
+fatal_inj_age_sex <- "ridagegen.xlsx"
+nonfatal_inj_summary <- "lfsinjsum.xlsx"
+nonfatal_inj_region <- "lfsinjreg.xlsx"
+nonfatal_inj_age <- "lfsinjage.xlsx"
+nonfatal_inj_ind <- "lfsinjind.xlsx"
+nonfatal_inj_occ <- "lfsinjocc.xlsx"
+
+nonfatal_tabname <- "Table-1"
+fatal_tabname <- "Table 1"
+
+nonfatal_header_row <- 6
+fatal_inj_headline_header_row <- 8
+fatal_inj_region_header_row <- 8
+fatal_inj_age_sex_header_row <- 9
+
+input_folder  <- "Example_Input"
+output_folder <- "Example_Output"
